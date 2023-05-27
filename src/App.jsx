4bff569e63d36b27296543cb1e46631dbb3938e4
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-27 10:44:44 +0400

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useState } from "react";
+import BtnContainer from "./BtnContainer";
+import JobsInfo from "./JobsInfo";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setJobs(data);
+      // console.log(data);
+    } catch (error) {}
+    setIsLoading(false);
+  };
+
+  useState(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobsInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
 };
 export default App;
